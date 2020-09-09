4d8fd0420f32e5a618b5785bb89f085ff0d9f7c8
Thiago Contardi <thiagocontardi@hotmail.com>
2020-09-09 16:33:23 -0300

    Changed Ubuntu docker install

---
+++ shell/enable-xdebug.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+usage() {
+  echo "Enable XDebug
+  Usage
+  $ shell/disable-sdebug.sh {version}
+   * version: PHP Version, e.g. php73
+  "
+}
+
+if [ "$1" == "-h" ] ; then
+  usage;
+  exit 0;
+fi
+
+if [ "$1" == "help" ] ; then
+  usage;
+  exit 0;
+fi
+
+VERSION="php74"
+
+if [ $# -eq 1 ]; then
+  VERSION=$1
+fi
+
+docker-compose exec ${VERSION}-fpm mv /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini.old /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini
+docker-compose restart ${VERSION}-fpm
--- shell/ubuntu.sh
+++ shell/ubuntu.sh
@@ -1,16 +1,25 @@
 #!/bin/bash
 
 #Script to install Docker in Ubuntu machines with right permission
+sudo apt-get remove -y docker docker-engine docker.io containerd runc
 sudo apt-get update
-sudo apt-get remove -y docker docker-engine docker.io docker-compose
 
 sudo apt-get install -y \
     apt-transport-https \
     ca-certificates \
     curl \
+    gnupg-agent \
     software-properties-common
 
-sudo snap install docker
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io
 
 sudo usermod -aG docker $USER
 sudo usermod -aG www-data $USER
@@ -19,8 +28,6 @@ sudo usermod -aG www-data $USER
 su - $USER
 
 #Download docker-compose
-sudo curl -L https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+sudo curl -L https://github.com/docker/compose/releases/download/1.27.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
-#Prepare for Elasticsearch
-sudo sysctl -w vm.max_map_count=262144
