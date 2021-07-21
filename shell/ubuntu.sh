f5db94516f540d187fbc2ff3ff756eff287102d5
Thiago Contardi <thiago@bizcommerce.com.br>
2021-07-21 16:57:35 -0300

    Added new docker-compose version

--- shell/ubuntu.sh
+++ shell/ubuntu.sh
@@ -28,6 +28,6 @@ sudo usermod -aG www-data $USER
 su - $USER
 
 #Download docker-compose
-sudo curl -L https://github.com/docker/compose/releases/download/1.27.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
