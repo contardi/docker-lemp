c8b28d5355e5c3dfd93fb96dceec88de61e5559f
Thiago Contardi <thiagocontardi@hotmail.com>
2023-06-01 14:23:04 -0300

    feat: new setup script

--- shell/setup.sh
+++ shell/setup.sh
@@ -15,14 +15,14 @@ echo \
   "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
 sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
 sudo usermod -aG docker $USER
 sudo usermod -aG www-data $USER
 
 #Reload group preferences
 su - $USER
-
-sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 
 
