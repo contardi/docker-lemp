f12879763552e6da195dda9ce381a95692db8f4c
Thiago Contardi <thiagocontardi@hotmail.com>
2023-06-01 13:53:03 -0300

    feat: new setup script

---
+++ shell/setup.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#Script to install Docker in Ubuntu machines with right permission
+for pkg in docker.io docker-doc docker-compose podman-docker containerd runc; do sudo apt-get remove $pkg; done
+
+sudo apt-get update
+
+sudo apt-get install -y ca-certificates curl gnupg
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+sudo usermod -aG docker $USER
+sudo usermod -aG www-data $USER
+
+#Reload group preferences
+su - $USER
+
+
